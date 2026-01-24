85bd3abd93906a3163223e26c28e0c909e0958f9
lovebloodanddiamonds <blowyourbra1n@mail.ru>
2026-01-24 16:27:22 +0300

    Aster import bugs

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+from unicex import HyperliquidUniClient, Aster
+
+
+async def main() -> None:
+    """Main entry point for the application."""
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
