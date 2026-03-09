19aecffc2a78bdeb5e24e630a8f02e4320de18bd
lovebloodanddiamonds <blowyourbra1n@mail.ru>
2026-03-09 20:23:08 +0300

    Add tests to funding interval methid

---
+++ tests/funding_interval_test.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from unicex import Exchange, get_uni_client
+
+
+async def main() -> None:
+    """Main entry point for the application."""
+
+    for e in Exchange:
+        client = await get_uni_client(e).create()
+
+        try:
+            interval = await client.funding_interval()
+            print(f"{e}: funding_interval={interval}")
+        except NotImplementedError:
+            print(f"{e}: funding_interval is not implemented")
+
+        await client.close_connection()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
